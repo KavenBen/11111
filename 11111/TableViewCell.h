70a6bc888f9511cc5f7738b44e674620646e73a7
KavenBen <1455660301@qq.com>
2016-06-02 17:30:58 +0800

    2

---
+++ 11111/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  11111
+//
+//  Created by bwfstu on 16/6/2.
+//  Copyright © 2016年 bwfstu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
---
+++ 11111/TableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.m
+//  11111
+//
+//  Created by bwfstu on 16/6/2.
+//  Copyright © 2016年 bwfstu. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
