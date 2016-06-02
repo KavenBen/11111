5a61624a8d72c8867f75f812a12a7185a3dd6c7f
KavenBen <1455660301@qq.com>
2016-06-02 17:18:06 +0800

    1
    无

---
+++ 11111/11111/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  11111
+//
+//  Created by bwfstu on 16/6/2.
+//  Copyright © 2016年 bwfstu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 11111/11111/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  11111
+//
+//  Created by bwfstu on 16/6/2.
+//  Copyright © 2016年 bwfstu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+//    dfsasdfsadfasfsadfsad
+//    fadsfasfsafsaf
+    NSLog(@"我很好");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
